f2540e2002c2a06a9ccaf504caaa9715892b8fac
Ryan Hung <ryanhungryan@gmail.com>
2024-04-10 23:24:26 +0100

    update packages's vite config file

--- packages/main/vite.config.js
+++ packages/main/vite.config.js
@@ -1,41 +1,51 @@
-import {node} from '../../.electron-vendors.cache.json';
-import {join} from 'node:path';
+import {node} from '../../electron-vendors.config.json';
+import {join} from 'path';
+import {builtinModules} from 'module';
+
+import {defineConfig} from 'vite';
+import {loadAndSetEnv} from '../../scripts/loadAndSetEnv.mjs';
 
 const PACKAGE_ROOT = __dirname;
-const PROJECT_ROOT = join(PACKAGE_ROOT, '../..');
 
 /**
- * @type {import('vite').UserConfig}
+ * Vite looks for `.env.[mode]` files only in `PACKAGE_ROOT` directory.
+ * Therefore, you must manually load and set the environment variables from the root directory above
+ */
+loadAndSetEnv(process.env.MODE, process.cwd());
+
+/**
  * @see https://vitejs.dev/config/
  */
-const config = {
-  mode: process.env.MODE,
+export default defineConfig({
   root: PACKAGE_ROOT,
-  envDir: PROJECT_ROOT,
   resolve: {
     alias: {
       '/@/': join(PACKAGE_ROOT, 'src') + '/',
     },
   },
   build: {
-    ssr: true,
     sourcemap: 'inline',
     target: `node${node}`,
     outDir: 'dist',
     assetsDir: '.',
-    minify: process.env.MODE !== 'development',
+    minify: process.env.MODE === 'development' ? false : 'terser',
+    terserOptions: {
+      ecma: 2020,
+      compress: {
+        passes: 2,
+      },
+      safari10: false,
+    },
     lib: {
       entry: 'src/index.ts',
-      formats: ['es'],
+      formats: ['cjs'],
     },
     rollupOptions: {
+      external: ['electron', 'electron-devtools-installer', ...builtinModules],
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: '[name].cjs',
       },
     },
     emptyOutDir: true,
-    reportCompressedSize: false,
   },
-};
-
-export default config;
+});
