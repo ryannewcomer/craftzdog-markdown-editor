8433f989fae71b4941553661a7482bb285731cd4
Ryan Hung <ryanhungryan@gmail.com>
2024-04-10 23:22:42 +0100

    update preload packages vite

--- packages/preload/vite.config.js
+++ packages/preload/vite.config.js
@@ -1,41 +1,50 @@
-import {chrome} from '../../.electron-vendors.cache.json';
-import {preload} from 'unplugin-auto-expose';
-import {join} from 'node:path';
+import {chrome} from '../../electron-vendors.config.json';
+import {join} from 'path';
+import {builtinModules} from 'module';
+import {defineConfig} from 'vite';
+import {loadAndSetEnv} from '../../scripts/loadAndSetEnv.mjs';
 
 const PACKAGE_ROOT = __dirname;
-const PROJECT_ROOT = join(PACKAGE_ROOT, '../..');
 
 /**
- * @type {import('vite').UserConfig}
+ * Vite looks for `.env.[mode]` files only in `PACKAGE_ROOT` directory.
+ * Therefore, you must manually load and set the environment variables from the root directory above
+ */
+loadAndSetEnv(process.env.MODE, process.cwd());
+
+/**
  * @see https://vitejs.dev/config/
  */
-const config = {
-  mode: process.env.MODE,
+export default defineConfig({
   root: PACKAGE_ROOT,
-  envDir: PROJECT_ROOT,
+  resolve: {
+    alias: {
+      '/@/': join(PACKAGE_ROOT, 'src') + '/',
+    },
+  },
   build: {
-    ssr: true,
     sourcemap: 'inline',
     target: `chrome${chrome}`,
     outDir: 'dist',
     assetsDir: '.',
-    minify: process.env.MODE !== 'development',
+    minify: process.env.MODE === 'development' ? false : 'terser',
+    terserOptions: {
+      ecma: 2020,
+      compress: {
+        passes: 2,
+      },
+      safari10: false,
+    },
     lib: {
       entry: 'src/index.ts',
-      formats: ['es'],
+      formats: ['cjs'],
     },
     rollupOptions: {
+      external: ['electron', ...builtinModules],
       output: {
-        // ESM preload scripts must have the .mjs extension
-        // https://www.electronjs.org/docs/latest/tutorial/esm#esm-preload-scripts-must-have-the-mjs-extension
-        entryFileNames: '[name].mjs',
+        entryFileNames: '[name].cjs',
       },
     },
     emptyOutDir: true,
-    reportCompressedSize: false,
   },
-
-  plugins: [preload.vite()],
-};
-
-export default config;
+});
